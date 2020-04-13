03773e752f8d20e8c8f6d780418deb20948603e0
Nikola Tosic <tosicnikola10@gmail.com>
2020-04-13 14:26:31 +1000

    novpc option. more readme details. no named

--- bbb.cfhighlander.rb
+++ bbb.cfhighlander.rb
@@ -6,6 +6,9 @@ CfhighlanderTemplate do
     ComponentParam :Route53Zone, '', description: 'Route53 Zone name to create A record -e.g. example.com. Leave empty for manual DNS configuration. Omit trailing dot'
     ComponentParam :ElasticIP, '', description: 'Elastic IP Address to assign to BBB server. Will be created if non given'
     ComponentParam :ImageId, ami_id, type: 'AWS::EC2::Image::Id', description: 'AMI Id for Ubuntu 16.04 Server Machine Image. Default to cannonical image within template region'
+    unless render_vpc
+      ComponentParam 'SubnetIds', type: 'List<AWS::EC2::Subnet::Id>', description: 'Subnets to place ASG instances into'
+    end
   end
 
   Condition(:EIPNotProvided, FnEquals(Ref(:ElasticIP), ''))
@@ -13,14 +16,15 @@ CfhighlanderTemplate do
   Condition(:ZoneProvided, FnNot(FnEquals(Ref(:Route53Zone), '')))
 
   # render vpc with public subnets to place big blue instance in
-  Component template: 'simple-vpc', name: 'vpc'
+  Component template: 'simple-vpc', name: 'vpc' if render_vpc
 
 
   user_data = File.read "#{template_dir}/user_data.sh"
   Component template: 'simple-asg', name: 'asg', config: { 'user_data' => user_data } do
     # passing parameter values
     parameter name: 'ImageId', value: Ref(:ImageId)
-    parameter name: 'SubnetIds', value: FnGetAtt('vpc', 'Outputs.PublicA')
+    parameter name: 'SubnetIds', value: FnGetAtt('vpc', 'Outputs.PublicA') if render_vpc
+    parameter name: 'SubnetIds', value: FnJoin(',', Ref('SubnetIds')) unless render_vpc
     parameter name: :EIP, value: FnIf(:EIPProvided, Ref(:ElasticIP), Ref(:EIPResource))
 
     # extended parameters from default component
