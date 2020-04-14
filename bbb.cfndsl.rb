225ef8c15c8c5cc706c1f338cd8f210e8005c1c8
Nikola Tosic <tosicnikola10@gmail.com>
2020-04-14 19:38:35 +1000

    backups, wait for completion, cleanup, tagging, admin account and ssm

--- bbb.cfndsl.rb
+++ bbb.cfndsl.rb
@@ -15,6 +15,32 @@ CloudFormation do
     TTL 3600
   end
 
+  CloudFormation_WaitCondition(:WaitSetupComplete) do
+    Count 1
+    Timeout 1800
+    Handle Ref(:WaitSetupCompleteHandle)
+  end
+
+  CloudFormation_WaitConditionHandle(:WaitSetupCompleteHandle) {}
+
+  IAM_Role(:RestoreRole) do
+    Path '/'
+    AssumeRolePolicyDocument service_assume_role_policy('backup')
+    ManagedPolicyArns %w(arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForRestores)
+  end
+  Backup_BackupSelection(:BackupSelection) do
+    BackupPlanId FnGetAtt('backup', 'Outputs.BackupPlanId')
+    BackupSelection ({
+        IamRoleArn: FnGetAtt(:RestoreRole, 'Arn'),
+        SelectionName: 'bbb-backup-selection',
+        ListOfTags: [{
+            ConditionKey: "ec2:ResourceTag/#{backup_tag_key}",
+            ConditionType: 'STRINGEQUALS',
+            ConditionValue: "#{tags[backup_tag_key]}"
+        }]
+    })
+  end
+
   Output(:ServerUrl) do
     Value(FnSub('https://${DomainName}'))
   end
--- user_data.sh
+++ user_data.sh
@@ -7,7 +7,9 @@ ip_address="${EIP}"
 set -x
 
 # install aws cli
-apt-get update && apt-get install -y awscli cloud-guest-utils
+apt-get update && \
+ apt-get install -y awscli cloud-guest-utils  python3-pip && \
+ pip3 install awscli --upgrade
 
 # get EIP association status and dissacoite if required
 instance_id=$(ec2metadata --instance-id)
@@ -28,3 +30,22 @@ mkdir -p /tmp/bbb-install && \
     wget https://ubuntu.bigbluebutton.org/bbb-install.sh  && \
     chmod a+x bbb-install.sh && \
     ./bbb-install.sh -v xenial-220 -s ${DomainName} -e ${AdminEmail} -l -g
+
+# create default admin password
+ADMIN_PASWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${!1:-32} | head -n 1)
+docker exec -e "ADMIN_PASSWORD=${!ADMIN_PASWORD}" greenlight-v2 bundle exec rake user:create[Admin,${AdminEmail},"${!ADMIN_PASSWORD}",admin,]
+
+# user may be already present, hence rendering the data only if previous command succeeds.
+if [[ $? == "0" ]]; then
+    aws configure set cli_follow_urlparam false
+    aws ssm put-parameter --name ${ConfigSSMPath}/admin_password --value "${!ADMIN_PASWORD}" --type SecureString --key-id 'alias/aws/ssm' --overwrite
+    aws ssm put-parameter --name ${ConfigSSMPath}/admin_user --value "${AdminEmail}" --type String  --overwrite
+    aws ssm put-parameter --name ${ConfigSSMPath}/server_url --value "https://${DomainName}" --type String  --overwrite
+fi
+echo "BigBlueButton server setup complete!!!"
+
+HANDLE_ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${!1:-32} | head -n 1)
+echo "Signalling setup complete to CFN using id ${!HANDLE_ID}"
+curl -X PUT -H 'Content-Type:' --data-binary \
+    "{\"Status\" : \"SUCCESS\",\"Reason\" : \"BBB Configuration Complete\",\"UniqueId\" : \"${!HANDLE_ID}\",\"Data\" : \"Application has completed configuration.\"}" \
+    "${WaitHandle}"
